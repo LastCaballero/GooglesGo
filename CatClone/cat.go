5f86d7af2f84fc382a142391bc4320a24be9361f
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2023-01-29 19:16:31 +0100

    Add files via upload

---
+++ CatClone/cat.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	
+)
+
+func do_stdin() {
+	st_scanner := bufio.NewScanner(os.Stdin)
+	for st_scanner.Scan() {
+		fmt.Println(st_scanner.Text())
+	}
+}
+
+func do_files() {
+	for _, filename := range os.Args[1:] {
+		bytes, err := os.ReadFile(filename)
+		if err == nil {
+			fmt.Printf(string(bytes))
+		}
+	}
+}
+
+func main() {
+	do_stdin()
+	do_files()
+}
